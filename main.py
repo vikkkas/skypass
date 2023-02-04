2627467bc50c5337aca56c8dca2add52c82a5390
vikkkas <vp030217@gmail.com>
2023-02-04 10:04:09 +0530

    Updated main code with respect to flight search module

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from data_manager import DataManager
-
+from flight_search import FlightSearch
+from datetime import datetime, timedelta
 
 data_manager = DataManager()
-
+sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
 
 ORIGIN_CITY_IATA = input('Enter your Departure airport IATA Code.')
 
-data_manager.get_destination_data()
 data_manager.get_destination_cities()
 data_manager.post_destination_cities()
+
+if sheet_data[0]["iataCode"] == "":
+    for row in sheet_data:
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+    data_manager.destination_data = sheet_data
+    data_manager.update_destination_codes()
+
+tomorrow = datetime.now()+timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6*30))
+
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+
+
